28836d2dc5af676877f1a506fddf8393a1768a4e
Ananna97 <ana_iancu97@yahoo.com>
2024-06-27 20:03:32 +0300

    l

--- booking/src/main/java/com/awbd/booking/config/SwaggerConfig.java
+++ booking/src/main/java/com/awbd/booking/config/SwaggerConfig.java
@@ -1,18 +1,38 @@
 package com.awbd.booking.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info().title("Spring Cloud application API")
-                .version("1").description("demo Spring Cloud"));
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.companyname.app"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiInfoMetaData());
     }
 
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("NAME OF SERVICE")
+                .description("API Endpoint Decoration")
+                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 
 }
